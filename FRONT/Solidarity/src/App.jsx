949fa68237428de9053d99e46207d1f06ce8c1e5
AndersJoel12 <anderabreu1@gmail.com>
2025-12-13 19:14:32 -0400

    Dividido el app para trabajar en conjunto

--- FRONT/Solidarity/src/App.jsx
+++ FRONT/Solidarity/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import Landing from './views/Landing'
 import './App.css'
+import Donations from './components/Donations_panel'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
+    <Donations/>
+    <br></br>
     <Landing/>
+    </>
   )
 }
 
