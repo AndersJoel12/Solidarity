77574cee84d49506f2a4cda1bea03f8fa3764bb1
AndersJoel12 <anderabreu1@gmail.com>
2025-12-14 20:04:07 -0400

    Contratos terminados en truffles

--- FRONT/Solidarity/src/App.jsx
+++ FRONT/Solidarity/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import Landing from './views/Landing'
 import './App.css'
 import Donations from './components/Donations_panel'
+import Contrato from './ConexionSol'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Contrato/>
+    <br />
     <Donations/>
     <br></br>
     <Landing/>
